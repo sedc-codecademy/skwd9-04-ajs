0a0547c7e8bfd68a63a57059a8411b3942cc6c75
Viktor Jakovlev <vicebad@hotmail.com>
2021-02-08 19:33:52 +0100

    class03 updates

--- G1/class03 - AJAX/code/script.js
+++ G1/class03 - AJAX/code/script.js
@@ -16,24 +16,57 @@ let acadmeyJsonToJSObject = JSON.parse(academyJson)
 //console.log(acadmeyJsonToJSObject)
 
 //plain Javascript AJAX request
-let request = new XMLHttpRequest();
-let method = "GET";
-let url = "https://jsonplaceholder.tyicode.com/users";
+// let request = new XMLHttpRequest();
+// let method = "GET";
+// let url = "https://jsonplaceholder.tyicode.com/users";
 
-request.open(method, url);
+// request.open(method, url);
 
-request.onload = function() {
-    //console.log(request.status)\
-    if (request.status >= 200 && request.status < 300) {
-        //console.log(request.response)
-        //console.log(typeof(request.response))
-        let parsedResult = JSON.parse(request.response)
-        console.log(parsedResult)
-        ///console.log(typeof(parsedResult))
-        console.log("success")
-    } else {
-        console.log("error")
-    }
-}
+// request.onload = function() {
+//     console.log(request.status)\
+//     if (request.status >= 200 && request.status < 300) {
+//         console.log(request.response)
+//         console.log(typeof(request.response))
+//         let parsedResult = JSON.parse(request.response)
+//         console.log(parsedResult)
+//         console.log(typeof(parsedResult))
+//         console.log("success")
+//     } else {
+//         console.log("error")
+//     }
+// }
 
-request.send();
+// request.send();
+
+//jquery AJAX request
+$(document).ready(function(){
+    let button = $("#get-data")
+    let display = $("#display")
+
+    let myObject = {}
+
+    button.click(function() {
+        $.ajax({
+            url: "https://swapi.dev/api/people/4/",
+            method: "GET",
+            success: function(response) {
+                console.log("success")
+                //console.log(typeof(response))
+                //console.log(response)
+                myObject = response
+                console.log(myObject)
+            },
+            error: function(response) {
+                console.log("error")
+                console.log(response)
+            }
+        })
+
+        display.innerHTML += "";
+        display.innerHTML += `
+            <p>Name: ${myObject.name}</p>
+            <p>Height: ${myObject.height}</p>
+            <p>Homeworld: ${myObject.homeworld}</p>
+        `;
+    })
+})
