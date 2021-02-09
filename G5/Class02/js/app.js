6232b265d4ccfc71483eb7ed16d40761e73db9d2
Martin <panovski.martin93@gmail.com>
2021-02-09 21:10:33 +0100

    Class 02 demo code part 03 added

--- G5/Class02/js/app.js
+++ G5/Class02/js/app.js
@@ -7,6 +7,10 @@
 // 6. Select all the elements that you need to work with from the HTML!
 
 let todoList = document.getElementById('todo-list');
+let addBtn = document.getElementById('addBtn');
+let selectPriority = document.getElementById('priority');
+
+let priorityColors = ['red', 'orange', 'yellow'];
 
 
 function Todo(content, isFinished, priority) {
@@ -15,15 +19,27 @@ function Todo(content, isFinished, priority) {
     this.priority = priority;
 }
 
-let todo1 = new Todo('Buy some bread!', false, 2);
-let todo2 = new Todo('Get your dog for a walk!', true, 1);
-let todo3 = new Todo('Call John!', true, 0);
+let todo1 = new Todo('Buy some bread!', false, priorityColors[0]);
+let todo2 = new Todo('Get your dog for a walk!', true, priorityColors[1]);
+let todo3 = new Todo('Call John!', true, priorityColors[2]);
 
 let todos = [todo1, todo2, todo3];
+
+// Save the todos in the local storage
+// let todoString = JSON.stringify(todos);
+// localStorage.setItem('todos_list', todoString);
+
 console.log(todos);
 
-function markTodo() {
-    alert("Hi");
+
+function markTodo(checkBox) {
+    if (checkBox.checked) {
+        // Cross the text
+        checkBox.previousElementSibling.style.textDecoration = 'line-through';
+    } else {
+        // Uncross the text
+        checkBox.previousElementSibling.style.textDecoration = 'none';
+    }
 }
 
 function displayItems(todosArray) {
@@ -32,16 +48,16 @@ function displayItems(todosArray) {
     for (let todo of todosArray) {
         if (!todo.isFinished) {
             todoList.innerHTML += `
-            <div class="todo">
+            <div class="todo" style="background-color: ${todo.priority}">
                 <p class="todo-text">${todo.content}</p>
-                <input type="checkbox" id="checkbox_${i}" class="checkbox" onchange="markTodo()">
+                <input type="checkbox" id="checkbox_${i}" class="checkbox" onchange="markTodo(this)">
             </div>
             `;
         } else {
             todoList.innerHTML += `
-            <div class="todo">
+            <div class="todo" style="background-color: ${todo.priority}">
                 <p class="todo-text" style="text-decoration:line-through;">${todo.content}</p>
-                <input type="checkbox" id="checkbox_${i}" class="checkbox" checked onchange="markTodo()">
+                <input type="checkbox" id="checkbox_${i}" class="checkbox" checked onchange="markTodo(this)">
             </div>
             `;
         }
@@ -49,4 +65,19 @@ function displayItems(todosArray) {
     }
 }
 displayItems(todos);
+
+addBtn.addEventListener('click', function() {
+    // 1. Get value of the input field and the value of the priority level drop down
+    // 2. Create new instance of Todo constructor function 
+    // 3. Add the new todo into an array of todos => push()
+    // 4. Display all the todos into the html => done
+
+    let inputValue = document.getElementById('todo-input').value;
+    let priorityLevel = selectPriority.value;
+    let newTodo = new Todo(inputValue, false, priorityColors[priorityLevel]);
+
+    todos.push(newTodo);
+    displayItems(todos);
+
+});
 
