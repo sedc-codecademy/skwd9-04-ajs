5232d38092ab6002db3e614035fb5618ba4d0831
Ivo Kostovski <ivo.kostovski@gmail.com>
2021-02-17 21:00:26 +0100

    Blog post refactored

--- G7/Workshop-blog-post-2/scripts/app.js
+++ G7/Workshop-blog-post-2/scripts/app.js
@@ -1,75 +1,72 @@
 // SELECTORS
 // Links
-let links = document.getElementsByClassName('nav-link');
+const links = Array.from(document.getElementsByClassName('nav-link'));
 
 // Pages
-let homePage = document.getElementById('home-page');
-let formPage = document.getElementById('form-page');
+const homePage = document.getElementById('home-page');
+const formPage = document.getElementById('form-page');
 
 // Elements
-let loader = document.getElementById('loader');
-let bpContainer = document.getElementById('blog-posts-container');
+const loader = document.getElementById('loader');
+const bpContainer = document.getElementById('blog-posts-container');
 
 // DATA
-let baseUrl = 'https://jsonplaceholder.typicode.com';
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 let blogPosts = [];
 
 // FUNCTIONS
-
-function changePageInView(page) {
+const changePageInView = page => {
     switch (page) {
         case 'nav-home':
-            homePage.style.display = 'block'; // show this element
-            formPage.style.display = 'none'; // hide this element
-            links[1].classList.remove('active'); // make this nav link inactive
-            links[0].classList.add('active'); // make this nav link active
+            changeDisplay(homePage, formPage, links[1], links[0]);
             break;
         case 'nav-form':
-            homePage.style.display = 'none';
-            formPage.style.display = 'block';
-            links[0].classList.remove('active');
-            links[1].classList.add('active');
+            changeDisplay(formPage, homePage, links[0], links[1]);
             break;
         default:
-            homePage.style.display = 'block';
-            formPage.style.display = 'none';
-            links[1].classList.remove('active');
-            links[0].classList.add('active');
+            changeDisplay(homePage, formPage, links[1], links[0]);
             break;
     }
 }
 
-function getPosts() {
-    loader.style.display = 'block';
+const changeDisplay = (showEl, hideEl, activeLink, inactiveLink) => {
+    if (showEl) {
+        showEl.style.display = 'block';
+    }
+    if (hideEl) {
+        hideEl.style.display = 'none';
+    }
+    if (activeLink) {
+        activeLink.classList.remove('active');
+    }
+    if (inactiveLink) {
+        inactiveLink.classList.add('active');
+    }
+}
+
+const getPosts = () => {
+    changeDisplay(loader);
 
     fetch(`${baseUrl}/posts`)
-    .then(function (res) {
+    .then(res => {
         if (res.ok) {
             return res.json()
         } else {
             throw new Error(res.status);
         }
     })
-    .then(function (bps) {
-        for (const bp of bps) {
-            let newBp = new BlogPost(bp.id, bp.userId, bp.title, bp.body)
-            blogPosts.push(newBp);
-        }
+    .then(bps => {
+        blogPosts = bps.map(bp => new BlogPost(bp.id, bp.userId, bp.title, bp.body));
         renderBlogPosts();
-
-        loader.style.display = 'none';   
-        console.log(blogPosts)
+        changeDisplay(null, loader);
     })
-    .catch(function (error) {
-        loader.style.display = 'none';
-        console.log(error)
-    })
+    .catch(() => changeDisplay(null, loader))
 }
 
-function renderBlogPosts() {
+const renderBlogPosts = () => {
     bpContainer.innerHTML = '';
 
-    for (const blogPost of blogPosts) {
+    blogPosts.forEach(blogPost => {
         bpContainer.innerHTML += 
         `
             <div class="col-sm mt-2">
@@ -83,27 +80,14 @@ function renderBlogPosts() {
               </div>
             </div>
         `
-    }
+    })
 }
-
-function init() {
-    setNavEventHandlers();
-    changePageInView();
-    getPosts();
-}
-
 
 // EVENT HANDLERS
-function setNavEventHandlers() {
-    for (let link of links) {
-        link.addEventListener('click', function (e) {
-            changePageInView(e.target.id);
-        })
-    }
-}
+const setNavEventHandlers = () =>
+    links.forEach(link => link.addEventListener('click', e => changePageInView(e.target.id)))
 
 // MODELS
-
 function BlogPost(id, userId, title, body) {
     this.id = id;
     this.userId = userId;
@@ -111,4 +95,10 @@ function BlogPost(id, userId, title, body) {
     this.body = body;
 }
 
-init();
+// Initialization
+
+(() => {
+    setNavEventHandlers();
+    changePageInView();
+    getPosts();
+})()
