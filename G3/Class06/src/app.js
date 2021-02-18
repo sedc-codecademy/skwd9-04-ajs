9b8bd4b7cb6b11dedea9b139b7cbf72cc35d4c91
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-02-18 20:48:45 +0100

    Class06 final-part

--- G3/Class06/src/app.js
+++ G3/Class06/src/app.js
@@ -5,33 +5,53 @@ let navigationService = {
     nextBtn: document.getElementById("nextBtn"),
     prevBtn: document.getElementById("prevBtn"),
     currentPage: 1,
+    pageType: "",
 
     // methods/functions
     init: function () {
         this.peopleBtn.addEventListener("click", function () {
-            console.log("Hello from people");
+            //console.log("Hello from people");
             navigationService.currentPage = 1;
             uiService.toggleLoader(true);
             starWarsService.getPeople(navigationService.currentPage);
+            navigationService.pageType = "people";
         });
         
         this.shipsBtn.addEventListener("click", function () {
-            console.log("Hello from ships");
+            //console.log("Hello from ships");
+            navigationService.currentPage = 1;
+            uiService.toggleLoader(true);
+            starWarsService.getShips(navigationService.currentPage);
+            navigationService.pageType = "ships";
         });
 
         this.nextBtn.addEventListener("click", function () {
-            console.log("Hello from next");
+            //console.log("Hello from next");
             uiService.toggleLoader(true);
             navigationService.currentPage += 1;
-            starWarsService.getPeople(navigationService.currentPage);
+
+            if (navigationService.pageType === "people") {
+                starWarsService.getPeople(navigationService.currentPage);
+            }
+            if (navigationService.pageType === "ships") {
+                starWarsService.getShips(navigationService.currentPage);
+            }
+
             console.log(navigationService.currentPage);
         });
 
         this.prevBtn.addEventListener("click", function () {
-            console.log("Hello from prev");
+            //console.log("Hello from prev");
             uiService.toggleLoader(true);
             navigationService.currentPage -= 1;
-            starWarsService.getPeople(navigationService.currentPage);
+
+            if (navigationService.pageType === "people") {
+                starWarsService.getPeople(navigationService.currentPage);
+            }
+            if (navigationService.pageType === "ships") {
+                starWarsService.getShips(navigationService.currentPage);
+            }
+
         });
     },
     togglePagingButtons: function (response) {
@@ -59,8 +79,8 @@ let starWarsService = {
         $.ajax({
             url: peopleUrl,
             success: function (response) {
-                console.log("Request success");
-                console.log(response);
+                //console.log("Request success");
+                //console.log(response);
                 uiService.displayPeopleInfo(response.results);
                 navigationService.togglePagingButtons(response);
                 uiService.toggleLoader(false);
@@ -70,6 +90,18 @@ let starWarsService = {
                 console.log(response)
             }
         });
+    },
+    getShips: function (page) {
+        let shipUrl = `${this.baseUrl}starships/?page=${page}`;
+        fetch(shipUrl)
+            .then(response => response.json())
+            .then(data => {
+                //console.log(data);
+                uiService.displayShipsInfo(data.results);
+                navigationService.togglePagingButtons(data);
+                uiService.toggleLoader(false);
+            })
+            .catch(err => console.log(err))
     }
 }
 
@@ -111,7 +143,58 @@ let uiService = {
             </div>
         `;
         }
+    },
+    displayShipsInfo: function (ships) {
+        this.result.innerHTML = "";
+        this.result.innerHTML += `
+            <div class="row yellow padding">
+                <div class="col-md-3">Name</div>
+                <div class="col-md-2">Model</div>
+                <div class="col-md-2">Manifacturer</div>
+                <div class="col-md-2">Cost</div>
+                <div class="col-md-2">Capacity</div>
+                <div class="col-md-1">Class</div>
+            </div>
+        `;
+
+        let parsedShips = ships.map(ship => {
+            let crewCapacity = parseInt(ship.crew);
+            let passengers = parseInt(ship.passengers);
+
+            let fullCapacity = 0;
+            if (!Number.isNaN(crewCapacity)) {
+                fullCapacity += crewCapacity;
+            }
+
+            if (!Number.isNaN(passengers)) {
+                fullCapacity += passengers;
+            }
+
+            return {
+                name: ship.name,
+                model: ship.model,
+                manufacturer: ship.manufacturer,
+                cost: ship.cost_in_credits,
+                capacity: fullCapacity,
+                shipClass: ship.starship_class
+            };
+        });
+        //console.log(parsedShips[0]);
+        for (let ship of parsedShips) {
+            this.result.innerHTML += `
+            <div class="row white padding">
+                <div class="col-md-3">${ship.name}</div>
+                <div class="col-md-2">${ship.model}</div>
+                <div class="col-md-2">${ship.manufacturer}</div>
+                <div class="col-md-2">${ship.cost}</div>
+                <div class="col-md-2">${ship.capacity}</div>
+                <div class="col-md-1">${ship.shipClass}</div>
+            </div>
+        `;
+        }
     }
 }
 
-navigationService.init();
+$(document).ready(function () {
+    navigationService.init();
+});
