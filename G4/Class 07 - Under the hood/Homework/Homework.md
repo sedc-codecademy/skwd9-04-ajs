cab92ac66bd593faeb0141ac9fffd76b31bb297e
Darko Panchevski <darko.panchevski@gmail.com>
2021-02-20 13:04:00 +0100

    Class 07 - G4

---
+++ G4/Class 07 - Under the hood/Homework/Homework.md
@@ -0,0 +1,16 @@
+# HomeWork
+
+There is a JSON file with students. Make a call to the file and get the following data from it: 
+* All students with an average grade higher than 3
+* All students with an average grade between 2 and 5
+* All female student names with an average grade of 5
+* All male student full names who live in Skopje and are over 18 years old
+* The average grades of all female students over the age of 24
+* All male students with a name starting with B and average grade over 2
+* The average grades of all female students over the age of 16 and who lives in Skopje and has "L" or "l" in their last name
+* Callback function that will return result of multiplication of two numbers
+* Callback function that will print the result of every user from "https://jsonplaceholder.typicode.com/users"
+
+Use higher order functions to find the answers
+**Link:** https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json
+
---
+++ G4/Class 07 - Under the hood/index.js
@@ -0,0 +1,143 @@
+// What is JavaScript?
+// A single-threaded non-blocking asynchronous concurrent
+// Single-threaded means that it has one threaded (it can do one thing at a time)
+// Blocking - do something until it's finished and then continue with the next peace of code
+
+// Simple function, setTimeout(), setInterval()
+const simpleFunction = () => console.log("Simple function");
+
+const functionWithTimeout = () => setTimeout(() => console.log("setTimeout and it will be done in 5 seconds"), 5000)
+
+const functionWithInterval = () => setInterval(() => console.log("setInterval and it will be done in 1 seconds"), 1000)
+
+
+simpleFunction();
+functionWithTimeout();
+functionWithInterval();
+
+// setTimeout() executes the function once on a time out. setInterval() executes the function repeatedly on and interval
+
+// Callback functions
+function calculate(callback, num1, num2){
+	console.log("calculating...");
+	return callback(num1,num2);
+};
+
+let result = calculate((x, y) => x + y, 2, 5);
+console.log("Result callback function", result);
+
+let students = [
+    { firstName: "Natasha", lastName: "Paneva", grade: 10, age: 23 },
+    { firstName: "Darko", lastName: "Panchevski", grade: 7, age: 25 },
+    { firstName: "Test", lastName: "Test", grade: 1, age: 19 },
+    { firstName: "Pero", lastName: "Perovski", grade: 5, age: 35 },
+    { firstName: "Blazo", lastName: "Blazovski", grade: 6, age: 99 }
+];
+
+const printStudentFullName = (callbackFunction, student) => {
+	console.log("callback function");
+	return callbackFunction(student);
+}
+
+students
+  .forEach(student => 
+    printStudentFullName(
+      student => 
+      console.log(
+        `${student.firstName} ${student.lastName}`
+      ),
+      student
+    )
+  );
+
+// Synchronous and asynchronous executing
+// Functions without delay
+function withoutDelay1() {
+    console.log("withoutDelay1");
+}
+
+function withoutDelay2() {
+    console.log("withoutDelay2");
+}
+
+withoutDelay1();
+withoutDelay2();
+
+// Functions with and without delay
+function first(){
+	setTimeout(() => console.log("First thing"), 1000);
+} 
+
+function second(){
+	setTimeout(() => console.log("Second thing"), 5000);
+}
+
+function third() {
+  console.log("Third thing");
+}
+
+function fourth() {
+  setTimeout(() => console.log("Fourth thing"), 2500);
+}
+
+first();
+second();
+third();
+fourth();
+
+// Two function calls but the first is delayed (solved with a callback)
+function first(callback) {
+    console.log("First thing");
+	setTimeout(()=>{
+        console.log("Log within setTimeout()");
+		callback();
+	}, 2500);
+}
+
+function second(){
+	console.log("Second thing!");
+}
+
+first(second);
+
+// making request using fetch
+function makeCall(url) {
+fetch(url)
+    .then(res => res.json())
+    .then(response => {
+            console.log('The request succeeded!');
+            return response;
+    })
+    .catch(error => {
+            console.log('The request failed!');
+            return response.responseText;
+    })
+}
+  
+function print(results){
+    console.log(results);
+}
+  
+print(makeCall("https://swapi.dev/api/people/1/")); // It will log undefined and The request succeeded!
+
+//  making request using fetch and callbacks
+function makeCall(url, callbackFunction){
+    fetch(url)
+      .then(res => res.json())
+      .then(response => {
+              console.log('The request succeeded!');
+              callbackFunction(response);
+      })
+      .catch(error => {
+              console.log('The request failed!');
+              callbackFunction(response.responseText);
+      })
+  }
+  
+  function print(results){
+       console.log(results);
+  }
+  
+  makeCall("https://swapi.dev/api/people/1/", print); // It will log The request succeeded! and the response result
+  
+// If you want to try it out check this page https://www.jsv9000.app/
