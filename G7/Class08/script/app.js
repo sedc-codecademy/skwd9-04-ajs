3ba0c47cbce51ff4354fb86e55e10755b9870dbb
Ivo Kostovski <ivo.kostovski@gmail.com>
2021-02-22 20:47:03 +0100

    Async await added

--- G7/Class08/script/app.js
+++ G7/Class08/script/app.js
@@ -44,28 +44,173 @@
 // Fulfilled
 // Rejected
 
-const first = workTime => {
+// const first = workTime => {
+//     return new Promise((resolve, reject) => {
+//         if (workTime <= 0) {
+//             reject(`It's too short of a work time. Please try again`);
+//         }
+//         setTimeout(() => {
+//             resolve('First log, preparing for the second one');
+//         }, workTime)
+//     })
+// }
+
+// const second = () => console.log('Second log');
+
+// first(-4)
+//     .then(success => {
+//         console.log('SUCCESS: ', success)
+//         second()
+//     })
+//     .catch(error => {
+//         console.log('ERROR', error)
+//     })
+//     .finally(() => {
+//         console.log(`Logging done at ${new Date()}`)
+//     })
+
+// Get documents example - version 1
+
+// const getDocuments = () => {
+//     return new Promise((resolve, reject) => {
+//         fetch('https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/G7/Class08/documents.json')
+//             .then(res => {
+//                 if (res.ok) {
+//                     return res.json();
+//                 } else {
+//                     throw new Error('ERROR occurred while getting the documents.')
+//                 }
+//             })
+//             .then(documents => resolve(documents))
+//             .catch(error => reject(error))
+//     })
+// }
+
+// const showDocuments = documents => {
+//     // making sure we have got an array
+//     if (!documents || !Array.isArray(documents)) {
+//         throw new Error('Problem with the documents')
+//     }
+
+//     // if we have documents inside the array (documents.length === 0)
+//     if (!documents.length) {
+//         throw new Error('There are no documents available');
+//     }
+
+//     // logging all the documents
+//     documents.forEach(doc => {
+//         console.log(`${doc.name}.${doc.type} (${doc.size}MB)`)
+//     });
+// }
+
+// let docs = [] // cannot be assigned a proper value
+
+// getDocuments()
+//     .then(documents => {
+//         // docs = documents;
+//         showDocuments(documents)
+//     })
+//     .catch(error => {
+//         console.log('ERROR: ', error)
+//     })
+
+// showDocuments(docs) //cannot be used here, as it will execute immediately
+
+// Get documents example - version 2
+
+const getDocuments = () => {
     return new Promise((resolve, reject) => {
-        if (workTime <= 0) {
-            reject(`It's too short of a work time. Please try again`);
-        }
-        setTimeout(() => {
-            resolve('First log, preparing for the second one');
-        }, workTime)
+        fetch('https://raw.githubusercontent.com/sedc-codecademy/skwd91-04-ajs/main/G7/Class08/documents.json')
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw new Error('ERROR occurred while getting the documents.')
+                }
+            })
+            .then(documents => resolve(documents))
+            .catch(error => reject(error))
     })
 }
 
-const second = () => console.log('Second log');
+const showDocuments = documents => {
+    // logging all the documents
+    documents.forEach(doc => {
+        console.log(`${doc.name}.${doc.type} (${doc.size}MB)`)
+    });
+}
 
-first(-4)
-    .then(success => {
-        console.log('SUCCESS: ', success)
-        second()
+const checkDocuments = documents => {
+    // making sure we have got an array
+    if (!documents || !Array.isArray(documents)) {
+        throw new Error('Problem with the documents')
+    }
+
+    // if we have documents inside the array (documents.length === 0)
+    if (!documents.length) {
+        throw new Error('There are no documents available');
+    }
+}
+
+const getImportantDocuments = documents => {
+    const importantDocuments = documents.filter(doc => doc.important) // doc.important === true
+    return new Promise((resolve, reject) => {
+        if (!importantDocuments.length) {
+            reject('There are no important documents!')
+        }
+        setTimeout(() => resolve(importantDocuments), 2000)
     })
-    .catch(error => {
-        console.log('ERROR', error)
-    })
-    .finally(() => {
-        console.log(`Logging done at ${new Date()}`)
-    })
+}
+
+// getDocuments()
+//     .then(documents => {
+//         checkDocuments(documents);
+//         return getImportantDocuments(documents)
+//     })
+//     .then(importantDocuments => {
+//         showDocuments(importantDocuments)
+//     })
+//     .catch(error => {
+//         console.log('ERROR: ', error)
+//     })
+
+// Async Await 
+
+const showImportantDocuments = async () => {
+    try {   
+        const documents = await getDocuments();
+        checkDocuments(documents);
+        const importantDocuments = await getImportantDocuments(documents);
+        showDocuments(importantDocuments);
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(`All done at ${new Date()}`);
+}
+
+// async function showImportantDocuments() {
+//     try {   
+//         const documents = await getDocuments();
+//         checkDocuments(documents);
+//         const importantDocuments = await getImportantDocuments(documents);
+//         showDocuments(importantDocuments);
+//     } catch (error) {
+//         console.log(error)
+//     }
+//     console.log(`All done at ${new Date()}`);
+// }
+
+showImportantDocuments();
+
+
+
+
+
+
+
+// Async await with function keyword
+
+// Async await with arrow functions
+
+
 
