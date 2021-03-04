05ac3df9d93c22080367ec13ab6ded8ff0eaf4e3
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2021-03-04 17:26:01 +0100

    added code for Object freeze, seal

--- G5/Class10/Code/js/objects.js
+++ G5/Class10/Code/js/objects.js
@@ -1,84 +1,98 @@
 // let person = new Object() // The main Object() constructor in JS
 
-
-// Create method 
+// Create method
 
 let dog = {
-    isHappy: true,
-    bark: function() {
-        console.log('BARK BARK!!!')
-    }
-}
+  isHappy: true,
+  bark: function () {
+    console.log("BARK BARK!!!");
+  },
+};
 
 let barnie = Object.create(dog);
 console.log(barnie);
-barnie.name = 'Barnie';
-barnie.color = 'white',
-barnie.age = 2;
+barnie.name = "Barnie";
+(barnie.color = "white"), (barnie.age = 2);
 console.log(barnie);
 
 let barnieTwinBrother = Object.create(barnie);
-barnieTwinBrother.name = 'Spyke';
+barnieTwinBrother.name = "Spyke";
 console.log(barnieTwinBrother);
-
-
 
 // Assign method
 
 let addressInfo = {
-    street: 'Dogge Street',
-    streetNumber: 24,
-    contactPerson: '070222111'
-}
+  street: "Dogge Street",
+  streetNumber: 24,
+  contactPerson: "070222111",
+};
 
 let barnieChip = Object.assign(barnie, addressInfo);
 console.log(barnieChip);
 
 let anotherAddress = {
-    street: 'Other Street',
-    streetNumber: 12,
-    contactPerson: '072222333'
-}
+  street: "Other Street",
+  streetNumber: 12,
+  contactPerson: "072222333",
+};
 
+console.log("BEFORE:", barnieChip);
 let barnieChipUpdate = Object.assign(barnieChip, anotherAddress);
+console.log("AFTER:", barnieChip);
 console.log(barnieChipUpdate);
 
 let barnieChipUpdateReversed = Object.assign(anotherAddress, barnieChip);
 console.log(barnieChipUpdateReversed);
 
+// What happened?
+const o1 = { a: 1 };
+const o2 = { b: 2 };
+const o3 = { c: 3 };
 
-// KEYS and VALUES
+const obj = Object.assign(o1, o2, o3);
+console.log(obj); // { a: 1, b: 2, c: 3 }
+console.log(o1); // { a: 1, b: 2, c: 3 }, target object itself is changed.
 
-let anotherDog = {
-    name: 'Bucky',
-    age: 1,
-    bark: function() {
-        console.log('BARK!!!');
-    },
-    printProperties: function() {
-        Object.keys(this).forEach(key => console.log('PropName:' + key));
-    },
-    printValues: function() {
-        Object.values(this).forEach(val => console.log('PropValue:' + val));
-    }
-}
+////////////////
+// Create an obj to use as the prototype
 
-anotherDog.printProperties();
-anotherDog.printValues();
+let person = {
+  name: "Ivan",
+  sayHello: function () {
+    console.log(`${this.name} says hi!`);
+  },
+};
 
+let personFull = {
+  firstName: "Ivan",
+  lastName: "Lazarevski",
+  getFullName: function () {
+    return this.firstName + this.lastName;
+  },
+};
 
+let animal = {
+  species: "Dog",
+  breed: "Jack Russell",
+  name: "Ben",
+  nicknames: ["Thief"],
+  addNickname: function (nick) {
+    this.nicknames.push(nick);
+  },
+};
 
+animal.addNickname("Annoying");
+console.log(animal.nicknames);
 
+Object.seal(person);
+Object.freeze(personFull);
+Object.preventExtensions(animal);
 
-let ivo = {
-    name: 'Ivo Kostovski',
-    country: {
-        countryName: 'Macedonia',
-        capital: 'Skopje',
-    },
-    hobbies: ['Reading', 'Swimming']
-}
-
-// When destructuring an object the order of the props doesn't matter
-const { country: { countryName, capital }, name,  ...hobbies } = ivo;
-// console.log(hobbies['hobbies'][999]);
+/*
+                              seal()      freeze()        preventExtensions()
+    add new prop            no          no              no
+    edit prop value         yes         no              yes
+    delete a prop           no          no              yes
+    change descriptors      no          no              yes
+    reassign __proto__      no          no              no
+    */
