1c70a34107569169e53f30634546abf3010a2a93
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-03-09 21:11:25 +0100

    Class12 part-3

---
+++ G3/Class12/excercise.js
@@ -0,0 +1,147 @@
+// # Exercises
+// ## Exercise 1
+// Create 3 object templates. Academy, Student and Subject. The structure should be:
+// Academy
+// * Name - string
+// * Students - array of Students
+// * Subjects - array of Subjects
+// * Start - Date when it starts
+// * End - Date when it ends
+// * NumberOfClasses - number of subjects * 10, not settable
+// * PrintStudents - method that prints all students in console
+// * PrintSubjects - method that prints all subjects in console
+// Subject
+// * Title - string
+// * NumberOfClasses - default 10, not settable
+// * isElective - boolean
+// * Academy - Academy object
+// * Students - array of Students
+// * OverrideClasses - accepts a number and rewrites the NumberOfClasses property with that number. 
+// The number can't be smaller than 3.
+// Student
+// * FirstName - string
+// * LastName - string
+// * Age - number
+// * CompletedSubjects - emptyArray as default, not settable
+// * Academy - null as default, not settable 
+// * CurrentSubject - null as default, not settable
+// * StartAcademy - accepts Academy object that it sets to the Academy property of the student
+// * StartSubject - accepts Subject object and adds it to the CurrentSubject property but only if the student 
+//has an Academy object in the Academy property and that subject exists in the academy.
+// If not, give error in console and do not set the CurrentSubject property
+// ## Exercise 2
+// Make the functions StartAcademy and StartSubject dynamic.
+// * StartAcademy - When the student calls StartAcademy, the student should also be added to the Academy property Students 
+// (The academy that he is starting )
+// * StartSubject - When the student calls StartSubject the student should also be added to the Subject property Students ( The subject that he is starting ). 
+// If there was another subject in the CurrentSubject property, that subject should be transferred to
+// CompletedSubjects and then add the new Subject
+
+//Homework from Class10
+//exercise 1/2 from class10
+function Academy(name, students, subjects, start, end) {
+    this.name = name;
+    this.students = students;
+    this.subjects = subjects;
+    this.start = start;
+    this.end = end;
+    this.numberOfClasses = function () {
+        console.log(this.subjects.length * 10);
+    }
+
+    this.printStudents = function () {
+        console.log(this.students);
+    };
+
+    this.printSubjects = function () {
+        console.log(this.subjects);
+    };
+
+};
+
+let instnaceAcademy = new Academy(
+    "SEDC",
+    ["Ana", "Petar", "Marija"],
+    ["Math", "English", "Biology", "Fizicko"],
+    new Date(10 / 02 / 2020), new Date(10 / 02 / 2021)
+);
+
+function Subject(title, isElective, students) {
+    this.title = title;
+    this.numberOfClasses = 10;
+    this.isElective = isElective;
+    this.academy = instnaceAcademy;
+    this.students = students;
+
+    this.overrideClasses = function (number) {
+        if (number === this.overrideClasses && number > 3) {
+            this.numberOfClasses = number
+        }
+    };
+};
+
+let instanceSubject = new Subject("Math", true, ["sara", "ana", "petar"]);
+//console.log(instanceSubject);
+
+function Student(firstName, lastName, age, completedSubjects) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.completedSubjects = completedSubjects;
+    this.academy = null;
+    this.currentSubject = null;
+
+    this.startAcademy = function () {
+        this.academy = instnaceAcademy;
+    };
+
+    // * StartSubject - accepts Subject object and adds it to the CurrentSubject property but only if the student 
+    // has an Academy object in the Academy property and that subject exists in the academy.
+
+    // * StartSubject - When the student calls StartSubject the student should also be added to the 
+    // Subject property Students(The subject that he is starting). 
+    // If there was another subject in the CurrentSubject property, that subject should be transferred to
+    // CompletedSubjects and then add the new Subject
+
+    this.startSubject = function (subject) {
+        if (this.academy) {
+            // let hasSub = false;
+            // for (const sub of this.academy.subjects) {
+            //     if (sub === subject.title) {
+            //         hasSub = true;
+            //         break;
+            //     }
+            // }
+            let hasSubject = this.academy.subjects.filter(sub => sub === subject.title).length > 0;
+            if (hasSubject) {
+                subject.students.push(this.firstName);
+                if (!this.currentSubject) {
+                    this.currentSubject = subject;
+                    return;
+                }
+
+                this.completedSubjects.push(this.currentSubject);
+                this.currentSubject = subject;
+            }
+        } else {
+            throw new Error("That subject does not exists in the current accademy!");
+        }
+    };
+};
+
+let trajan = new Student(
+    "Trajan",
+    "Stevkovski",
+    33,
+    [new Subject("Math", false, ["Filip", "Olga"]), new Subject("Science", true, ["Petar", "Angel"])]
+);
+trajan.startAcademy(instnaceAcademy);
+
+
+
+let biology = new Subject("Biology", true, ["sara", "ana", "petar"]);
+trajan.startSubject(biology);
+
+let fizicko = new Subject("Fizicko", false, ["sara_", "ana_", "petar_"]);
+trajan.startSubject(fizicko);
+console.log(trajan);
--- G3/Class12/script.js
+++ G3/Class12/script.js
@@ -78,17 +78,31 @@ class Car extends WheeledVehicle {
         }
     }
 
-    static addAc() {
-        console.log("Im static method");
+    static addAc(car) {
+        if (!car.hasAc) {
+            car.hasAc = true;
+            car.price += 200;
+            console.log(`The car has AC now and it costs ${car.price}`);
+        } else {
+            console.log("The car already has AC!");
+        }
     }
+
+    static checkHowMuchDoorItHas(car) {
+        if (car.doors >= 4) {
+            console.log("Has 4 doors");
+        } else if (car.doors >= 2) {
+            console.log("Its a sports car");
+        } else {
+            console.log("Its a caravan");
+        }
+    }  
 }
 
-Number.isNaN(3);
-
-Car.addAc()
-let zastava = new Car(1, "101", "asd123", 500, 2, false);
-zastava.add
+// let zastava = new Car(1, "101", "asd123", 500, 2, false);
 // console.log(zastava);
+// Car.addAc(zastava);
+// Car.checkHowMuchDoorItHas(zastava);
 // let fiat = new Car(2, "Uno", "asdzxc", 700, 4, true);
 // console.log(fiat);
 // let result = fiat.buyCar(700);
@@ -98,7 +112,61 @@ zastava.add
 // console.log(result1);
 // console.log(result1.message);
 
+class ElectricCar extends Car {
+    constructor(id, name, batch, price, doors, owner) {
+        super(id, name, batch, price, doors, true);
+        this.owner = owner;
+    }
 
+    get owner() {
+		console.log("We are getting the name of the owner. Please wait...");
+        return "The car is owned by:" + this._owner;
+    }
+	set owner(ownerName){
+		console.log("We are setting the name of the owner. Please wait...")
+		ownerName.length > 1 ? this._owner = ownerName : (()=> {throw new Error("Owner name too short!")})();
+	}
+}
+
+class DiselCar extends Car {
+    constructor(id, name, batch, price, doors, owner) {
+        super(id, name, batch, price, doors, true);
+        this.owner = owner;
+    }
+}
+
+let tesla = new ElectricCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+
+// console.log(tesla);
+// console.log(tesla.owner);
+
+// let tesla2 = new ElectricCar(2, "Tesla2", "12a2", 25000, 2, "T");
+
+// console.log(tesla instanceof ElectricCar);
+// console.log(tesla instanceof Vehicle);
+// console.log(tesla instanceof DiselCar);
+
+let tesla1 = new ElectricCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let tesla2 = new ElectricCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let tesla3 = new ElectricCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let tesla4 = new ElectricCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+
+let diesel = new DiselCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let diesel2 = new DiselCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let diesel3 = new DiselCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+let diesel4 = new DiselCar(2, "Tesla", "12a", 30000, 5, "Trajan");
+
+let cars = [tesla1, tesla2, tesla3, tesla4, diesel, diesel2, diesel3, diesel4];
+
+for (let car of cars) {
+    if (car instanceof ElectricCar) {
+        console.log("ElectricCar");
+    }
+
+    if (car instanceof DiselCar) {
+        console.log("DiselCar");
+    }
+}
 
 // constructor with efault parameters
 // class Car extends WheeledVehicle {
